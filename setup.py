601698b786dd9adc3b6d8befe2252d314ab8cec1
dantezhu <dantezhu@qq.com>
2014-06-20 21:56:52 +0800

    setup文件

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="haven",
+    version='1.0.1',
+    zip_safe=False,
+    platforms='any',
+    packages=['haven'],
+    install_requires=['events', 'netkit'],
+    url="https://github.com/dantezhu/haven",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="flask's style binary server framework",
+)
