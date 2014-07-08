e51040c537bb57f48075552cc27d1ac21c9491f1
dantezhu <dantezhu@qq.com>
2014-07-08 17:41:09 +0800

    忘记改线程了

--- haven/thread_impl.py
+++ haven/thread_impl.py
@@ -89,8 +89,9 @@ class TLater(object):
 
             # 必须要确定，这次调用就是这个timer引起的
             if self.timer == timer:
+                self.timer = None
                 result = safe_call(callback)
-                if repeat and self.timer == timer:
+                if repeat and not self.timer:
                     # 之所以还要判断timer，是因为callback中可能设置了新的回调
                     self.set(interval, callback, repeat, True)
                 return result
