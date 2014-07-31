6feb408402a0d16b6b4ff751bf8b5753b51471f7
dantezhu <dantezhu@qq.com>
2014-08-01 01:30:41 +0800

    支持多进程

--- examples/ws_test/client.py
+++ examples/ws_test/client.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
 from websocket import create_connection
 from kola_box import KolaBox
 
 ws = create_connection("ws://127.0.0.1:8000/echo")
 # ws = create_connection("ws://115.28.224.64:8000/echo")
-print "Sending 'Hello, World'..."
 box = KolaBox()
 box.set_json(dict(
     endpoint=1
 ))
+t1 = time.time()
 ws.send(box.pack())
-print "Sent"
-print "Reeiving..."
 result = ws.recv()
+print 'time past: ', time.time() - t1
 print "Received '%r'" % result
 ws.close()
--- examples/ws_test/gun_config.py
+++ examples/ws_test/gun_config.py
@@ -4,7 +4,7 @@ proc_name = 'server'
 # sync/gevent
 # bind = ['0.0.0.0:8000']
 bind = ['127.0.0.1:8000']
-workers = 1
+workers = 2
 worker_class = GeventWebSocketWorker
 # for debug
 #accesslog = '-'
