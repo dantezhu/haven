8f12a1e55a12805c19c868347673f4517bb0f7b3
dantezhu <dantezhu@qq.com>
2014-08-01 19:58:23 +0800

    单独改为benchmark_test

--- examples/benchmark_test/reimp.py
+++ examples/benchmark_test/reimp.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import logging
-logger = logging.getLogger('main')
+
+from haven import GHaven as Haven, GBlueprint as Blueprint, GTimer as Timer
+# from haven import THaven as Haven, TBlueprint as Blueprint, GTimer as Timer
 
 #from netkit.box import Box as Box
 from kola_box import KolaBox as Box
+
+from haven import logger
 
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
---
+++ examples/benchmark_test/server.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from reimp import Haven, logger, Box
+
+app = Haven(Box)
+
+@app.route(101)
+def index(request):
+    request.write(dict(ret=0))
+
+app.run('127.0.0.1', 7777, workers=4)
