42a9044ba60cf45e1f7ab5db56ccd1d6371e0529
dantezhu <dantezhu@qq.com>
2014-08-04 20:51:01 +0800

    解决websocket recv到bytearray的问题

--- haven/contrib/websocket_gevent_impl.py
+++ haven/contrib/websocket_gevent_impl.py
@@ -15,7 +15,8 @@ from haven import GHaven
 class WSStream(Stream):
     def read_from_fd(self):
         try:
-            chunk = self.sock.receive()
+            # 如果二进制传输，获取的将会是bytearray，会导致box.body是bytearray格式，从而导致protobuf parse报错
+            chunk = str(self.sock.receive())
         except:
             logger.error('exc occur.', exc_info=True)
             # 其他都直接关闭
