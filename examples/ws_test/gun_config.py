5dd82d04871e85db1416e6e41e9a1ae7d82096e6
dantezhu <dantezhu@qq.com>
2016-01-12 09:50:16 +0800

    新的gunicorn19.x，config文件里面的worker_class需要是str

--- examples/ws_test/gun_config.py
+++ examples/ws_test/gun_config.py
@@ -1,11 +1,9 @@
-from geventwebsocket.gunicorn.workers import GeventWebSocketWorker
-
 proc_name = 'server'
 # sync/gevent
 # bind = ['0.0.0.0:8000']
 bind = ['127.0.0.1:8000']
 workers = 2
-worker_class = GeventWebSocketWorker
+worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
 # for debug
 #accesslog = '-'
 #loglevel = 'debug'
