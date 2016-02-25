39bbe4c7510823df6b0e2d53f5bb99251ee28e84
dantezhu <dantezhu@qq.com>
2016-02-26 02:21:38 +0800

    增加一个新的依赖 setproctitle

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    install_requires=['events', 'netkit'],
+    install_requires=['events', 'netkit', 'setproctitle'],
     url="https://github.com/dantezhu/haven",
     license="MIT",
     author="dantezhu",
