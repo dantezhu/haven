d173a29d9468bc09ed35adc2d8aa17d51f5d539a
dantezhu <dantezhu@qq.com>
2017-02-22 10:57:22 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="haven",
-    version='1.1.99',
+    version='1.1.101',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
