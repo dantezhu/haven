688e03fc0bb4024570898ed38a7d63f34f523f4e
dantezhu <dantezhu@qq.com>
2020-06-08 12:02:41 +0800

    >=3

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    python_requires='>=3',
     install_requires=['events', 'netkit', 'setproctitle'],
     url="https://github.com/dantezhu/haven",
     license="MIT",
