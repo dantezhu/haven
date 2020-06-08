247b3608ecb97e24c9f6faccdc5a95007e64935b
dantezhu <dantezhu@qq.com>
2020-06-08 12:29:24 +0800

    ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="haven",
-    version='2.0.1',
+    version='2.0.2',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
