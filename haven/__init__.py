76f58afd720a30bcc1bd2b104ede2d266a91a0ec
dantezhu <dantezhu@qq.com>
2020-07-15 10:47:36 +0800

    版本升级

--- haven/__init__.py
+++ haven/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 from .log import logger
 from .utils import safe_call, safe_func
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="haven",
-    version='2.0.2',
+    version='2.0.3',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
